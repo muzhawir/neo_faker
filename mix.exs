e0402e1c546e62e33bce124657867275697004c5
muzhawir <muzhawir.amri@gmail.com>
2025-02-07 04:21:44 +0800

    Bump version to 0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NeoFaker.MixProject do
   def project do
     [
       app: :neo_faker,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()

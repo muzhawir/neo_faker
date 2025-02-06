1daa04ba7eeb3b69e366201b9e0ff5ec0c4dddee
muzhawir <muzhawir.amri@gmail.com>
2025-02-07 04:11:51 +0800

    Update BloodTest module

--- test/neo_faker/blood_test.exs
+++ test/neo_faker/blood_test.exs
@@ -1,12 +1,24 @@
 defmodule NeoFaker.BloodTest do
   use ExUnit.Case
 
-  describe "full_format/0" do
+  describe "group/0" do
     test "returns a full format blood type" do
-      full_format = NeoFaker.Blood.full_format()
+      full_format = NeoFaker.Blood.group()
       blood_types = ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"]
 
       assert full_format in blood_types
+    end
+  end
+
+  describe "type/0" do
+    test "returns a blood type" do
+      assert NeoFaker.Blood.type() in ["A", "B", "AB", "O"]
+    end
+  end
+
+  describe "rh_factor/0" do
+    test "returns a rh factor" do
+      assert NeoFaker.Blood.rh_factor() in ["+", "-"]
     end
   end
 end

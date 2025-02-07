3bf82ded935275fef79aa07fa091ee41e565ba94
muzhawir <muzhawir.amri@gmail.com>
2025-02-08 02:51:47 +0800

    Fix typo

--- lib/nf/gravatar.ex
+++ lib/nf/gravatar.ex
@@ -1,6 +1,6 @@
 defmodule Nf.Gravatar do
   @moduledoc """
-  Functions to generating random Gravatar URLs based on email addresses.
+  Functions for generating random Gravatar URLs based on email addresses.
 
   This module is based on the [Gravatar API documentation](https://docs.gravatar.com/api/avatars/images).
   """

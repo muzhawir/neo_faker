0eb218ee6e26083a537285ad9eb72e602174b24e
muzhawir <muzhawir.amri@gmail.com>
2025-02-09 03:38:58 +0800

    Add Boolean module

---
+++ lib/nf/boolean.ex
@@ -0,0 +1,24 @@
+defmodule Nf.Boolean do
+  @moduledoc """
+  Functions for generating boolean values.
+  """
+  @moduledoc since: "0.5.0"
+
+  @doc """
+  Generate a boolean value.
+
+  Returns a boolean value, default true ratio is 50% if no argument is provided.
+
+  ## Examples
+
+      iex> Nf.Boolean.boolean()
+      true
+
+      iex> Nf.Boolean.boolean(50)
+      false
+
+  """
+  def boolean(true_ratio \\ 50) when true_ratio in 0..100 do
+    :rand.uniform() <= true_ratio / 100
+  end
+end
--- test/nf/boolean_test.exs
+++ test/nf/boolean_test.exs
@@ -1,3 +1,13 @@
 defmodule Nf.BooleanTest do
   use ExUnit.Case, async: true
+
+  describe "value" do
+    test "returns a boolean value" do
+      assert Nf.Boolean.boolean() in [true, false]
+    end
+
+    test "returns a boolean value with true_ratio" do
+      assert Nf.Boolean.boolean(50) in [true, false]
+    end
+  end
 end

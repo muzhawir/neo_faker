f8374c08ad1fbccc151d144017ce52d3ee64007c
muzhawir <muzhawir.amri@gmail.com>
2025-02-10 19:31:45 +0800

    Add l10n data

---
+++ lib/data/l10n/default/gender.exs
@@ -0,0 +1,16 @@
+%{
+  "binary" => ["Male", "Female"],
+  "short_binary" => ["M", "F"],
+  "non_binary" => [
+    "Agender",
+    "Androgyne",
+    "Bigender",
+    "Demigender",
+    "Genderfluid",
+    "Genderqueer",
+    "Neutrois",
+    "Non-binary",
+    "Pangender",
+    "Two-spirit"
+  ]
+}
---
+++ lib/data/l10n/id_id/gender.exs
@@ -0,0 +1,16 @@
+%{
+  "binary" => ["Laki-laki", "Perempuan"],
+  "short_binary" => ["L", "P"],
+  "non_binary" => [
+    "Agender",
+    "Androgini",
+    "Bigender",
+    "Demigender",
+    "Genderfluida",
+    "Genderkuir",
+    "Netral",
+    "Non-biner",
+    "Pangender",
+    "Dua roh"
+  ]
+}

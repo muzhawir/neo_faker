43b6ea100516616ec41f30bd8966e403de83d8c1
muzhawir <muzhawir.amri@gmail.com>
2025-02-10 19:30:54 +0800

    Add mix task for lint project

--- lib/mix/tasks/lint.ex
+++ lib/mix/tasks/lint.ex
@@ -1,5 +1,12 @@
 defmodule Mix.Tasks.Lint do
-  @moduledoc false
+  @shortdoc "Runs `mix format`, `mix test`, `mix dialyzer`, and `mix credo` sequentially."
+  @moduledoc """
+  Runs `mix format`, `mix test`, `mix dialyzer`, and `mix credo` sequentially to check code quality.
+
+  This task is required for contributors to run before submitting a pull request (PR).
+  Ensure all checks pass to maintain code consistency and correctness.
+  """
+  @moduledoc since: "0.5.0"
 
   use Mix.Task
 
@@ -7,40 +14,34 @@ defmodule Mix.Tasks.Lint do
     Mix.env(:test)
 
     width = terminal_width()
-    separator = String.duplicate("-", width)
+    separator = String.duplicate("_", width)
 
-    run_mix_format()
-    Mix.shell().info("\n#{separator}\n")
+    Mix.Task.run("format")
 
     run_mix_test()
-    Mix.shell().info("\n#{separator}\n")
+    Mix.shell().info("\n#{separator}")
 
     run_mix_dialyzer()
-    Mix.shell().info("\n#{separator}\n")
+    Mix.shell().info("\n#{separator}")
 
     run_mix_credo()
-    Mix.shell().info("\n#{separator}\n")
-  end
-
-  defp run_mix_format do
-    Mix.Task.reenable("format")
-    Mix.Task.run("format")
+    Mix.shell().info("\n#{separator}")
+    Mix.shell().info("\n⏹️ END LINT")
   end
 
   defp run_mix_test do
-    Mix.Task.reenable("test")
+    Mix.shell().info("\n▶️ BEGIN MIX TEST\n")
     Mix.Task.run("test")
   end
 
   defp run_mix_dialyzer do
-    Mix.Task.reenable("dialyzer")
+    Mix.shell().info("\n▶️ BEGIN MIX DIALYZER\n")
     Mix.Task.run("dialyzer", ["--quiet-with-result"])
   end
 
   defp run_mix_credo do
-    Mix.Task.reenable("credo")
+    Mix.shell().info("\n▶️ BEGIN MIX CREDO\n")
     Mix.Task.run("credo", ["--format", "oneline"])
-    Mix.shell().info("Mix Credo finished")
   end
 
   defp terminal_width do

b26a7ba345827adadf4eaf8b445c616ed8e072ce
muzhawir <muzhawir.amri@gmail.com>
2025-02-11 20:12:31 +0800

    Refactor Nf.Helper module

--- lib/nf/helper.ex
+++ lib/nf/helper.ex
@@ -1,33 +1,41 @@
 defmodule Nf.Helper do
-  @moduledoc """
-  This module provides helper functions commonly used across other modules.
-  """
-  @moduledoc since: "0.4.1"
+  @moduledoc false
 
   @doc """
-  Helper function for build locale path.
+  Builds the absolute path to a locale-specific file.
+
+  Given a `locale`, `module`, and `file_name`, this function constructs the
+  corresponding file path within the `lib/data/` directory.
   """
   @spec build_locale_path(String.t(), String.t(), String.t()) :: String.t()
   def build_locale_path(locale, module, file_name) do
     Path.join([File.cwd!(), "lib", "data", locale, module, file_name])
   end
 
-  def load_locale_file(path), do: path |> Code.eval_file() |> elem(0)
-
   @doc """
-  Reads a locale file and returns its contents as a map.
+  Reads a locale-specific file and returns its content as a map.
 
-  If the specified locale file does not exist, it falls back to the default locale.
+  This function determines the appropriate locale from the application
+  configuration and attempts to read the specified file. If the file is
+  not found for the configured locale, it falls back to the `"default"` locale.
   """
   @spec read_locale_file(String.t(), String.t()) :: map()
-  def read_locale_file(module, file_name) do
+  def read_locale_file(module_name, file_name) do
     locale = Application.get_env(:neo_faker, :locale)
-    path = build_locale_path(locale, module, file_name)
+    path = build_locale_path(locale, module_name, file_name)
 
     if File.exists?(path) do
       load_locale_file(path)
     else
-      load_locale_file(build_locale_path("default", module, file_name))
+      load_locale_file(build_locale_path("default", module_name, file_name))
     end
   end
+
+  @doc """
+  Loads and evaluates the given locale file.
+
+  This function reads and evaluates an `.exs` file from the given `path`,
+  returning the evaluated result.
+  """
+  def load_locale_file(path), do: path |> Code.eval_file() |> elem(0)
 end

b33bf1a3eb0aa36fbd32c9da8c249919a75d9db7
muzhawir <muzhawir.amri@gmail.com>
2025-02-14 05:34:09 +0800

    Refactor Crypto module

--- lib/nf/crypto.ex
+++ lib/nf/crypto.ex
@@ -27,11 +27,18 @@ defmodule Nf.Crypto do
   ## Examples
 
       iex> Nf.Crypto.md5()
-      "e35cb102765cfc56df21ba4c16e6a636"
+      "afc4c626c55e4166421d82732163857d"
+
+      iex> Nf.Crypto.md5(case: :upper)
+      "AFC4C626C55E4166421D82732163857D"
+
+      iex> Nf.Crypto.md5(2)
+      ["afc4c626c55e4166421d82732163857d",
+      "9b1c12d90d0aaf8d9b20458fbb24d481"]
 
   """
-  @spec md5(Keyword.t()) :: String.t()
-  def md5(opts \\ []), do: generate_hash(:md5, opts)
+  @spec md5(non_neg_integer(), Keyword.t()) :: String.t() | [String.t()]
+  def md5(amount \\ 1, opts \\ []), do: generate_hash(amount, :md5, opts)
 
   @doc """
   Generates a random SHA-1 hash.
@@ -43,11 +50,18 @@ defmodule Nf.Crypto do
   ## Examples
 
       iex> Nf.Crypto.sha1()
-      "c8719790cdfff41c37c75e0c848d2b57535255aa"
+      "339fe49a3fd882823ae29263e4873e1a6200ca69"
+
+      iex> Nf.Crypto.sha1(2)
+      ["339fe49a3fd882823ae29263e4873e1a6200ca69",
+      "c8719790cdfff41c37c75e0c848d2b57535255aa"]
+
+      iex> Nf.Crypto.sha1(case: :upper)
+      "339FE49A3FD882823AE29263E4873E1A6200CA69"
 
   """
-  @spec sha1(Keyword.t()) :: String.t()
-  def sha1(opts \\ []), do: generate_hash(:sha, opts)
+  @spec sha1(non_neg_integer(), Keyword.t()) :: String.t() | [String.t()]
+  def sha1(amount \\ 1, opts \\ []), do: generate_hash(amount, :sha, opts)
 
   @doc """
   Generates a random SHA-256 hash.
@@ -59,9 +73,16 @@ defmodule Nf.Crypto do
   ## Examples
 
       iex> Nf.Crypto.sha256()
-      "d0ff021e810fb8f3442a14393604b0661b02f0dfcb347d80c9580af3ab5e7e6c"
+      "0d2780031e5d677719534fd32d7418c191cc7cecfb392566d8e1206f94631469"
+
+      iex> Nf.Crypto.sha256(2)
+      ["0d2780031e5d677719534fd32d7418c191cc7cecfb392566d8e1206f94631469",
+      "d0ff021e810fb8f3442a14393604b0661b02f0dfcb347d80c9580af3ab5e7e6c"]
+
+      iex> Nf.Crypto.sha256(case: :upper)
+      "0D2780031E5D677719534FD32D7418C191CC7CECFB392566D8E1206F94631469"
 
   """
-  @spec sha256(Keyword.t()) :: String.t()
-  def sha256(opts \\ []), do: generate_hash(:sha256, opts)
+  @spec sha256(non_neg_integer(), Keyword.t()) :: String.t() | [String.t()]
+  def sha256(amount \\ 1, opts \\ []), do: generate_hash(amount, :sha256, opts)
 end
--- lib/nf/crypto/utils.ex
+++ lib/nf/crypto/utils.ex
@@ -9,7 +9,8 @@ defmodule Nf.Crypto.Utils do
 
   ## Parameters
 
-  - `hash_type` - The hashing algorithm to use (e.g., `:md5`, `:sha1`, `:sha256`).
+  - `amount` - The number of hashes to generate.
+  - `hash_type` - The hashing algorithm to use (e.g., `:md5`, `:sha`, `:sha256`).
   - `options` - A list of options to modify the hash output.
 
   ## Options
@@ -23,16 +24,18 @@ defmodule Nf.Crypto.Utils do
   - `:upper` - uses upper case characters
 
   """
-  @spec generate_hash(atom(), Keyword.t()) :: String.t()
-  def generate_hash(hash_type, []) do
-    data = :crypto.strong_rand_bytes(16)
+  @spec generate_hash(non_neg_integer(), atom(), Keyword.t()) :: String.t() | [String.t()]
+  def generate_hash(amount \\ 1, hash_type, opts \\ [])
+  def generate_hash(1, hash_type, opts), do: generate_hashed_value(hash_type, opts)
 
-    hash_type |> :crypto.hash(data) |> Base.encode16(case: :lower)
+  def generate_hash(amount, hash_type, opts) when amount > 1 do
+    for _ <- 1..amount, do: generate_hashed_value(hash_type, opts)
   end
 
-  def generate_hash(hash_type, case: :upper) do
+  defp generate_hashed_value(hash_type, opts) do
     data = :crypto.strong_rand_bytes(16)
+    case_type = Keyword.get(opts, :case, :lower)
 
-    hash_type |> :crypto.hash(data) |> Base.encode16()
+    hash_type |> :crypto.hash(data) |> Base.encode16(case: case_type)
   end
 end

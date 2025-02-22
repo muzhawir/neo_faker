c05d0ce46d25b8aecfc82214446869ebed57b172
muzhawir <muzhawir.amri@gmail.com>
2025-02-23 07:27:32 +0800

    Add options to NeoFaker.Boolean.boolean function

--- lib/neo_faker/boolean.ex
+++ lib/neo_faker/boolean.ex
@@ -10,7 +10,10 @@ defmodule NeoFaker.Boolean do
   Returns a random boolean value.
 
   By default, the function returns `true` or `false` with equal probability (50% each).
-  An optional argument can be provided to adjust the chance of returning `true`.
+  An optional `true_ratio` argument can be provided to adjust the chance of returning `true`.
+
+  If the `integer: true` option is set to `true`, the function returns `1` for `true` or `0`
+  for `false`.
 
   ## Examples
 
@@ -20,9 +23,22 @@ defmodule NeoFaker.Boolean do
       iex> NeoFaker.Boolean.boolean(75)
       true
 
+      iex> NeoFaker.Boolean.boolean(75, integer: true)
+      1
+
   """
-  @spec boolean(pos_integer()) :: boolean()
-  def boolean(true_ratio \\ 50) when true_ratio in 0..100 do
+  @spec boolean(pos_integer(), Keyword.t()) :: boolean()
+  def boolean(true_ratio \\ 50, opts \\ [])
+
+  def boolean(true_ratio, []) when true_ratio in 0..100 do
     :rand.uniform() <= true_ratio / 100
   end
+
+  def boolean(true_ratio, integer: true) when true_ratio in 0..100 do
+    result = :rand.uniform() <= true_ratio / 100
+
+    if result == true, do: 1, else: 0
+  end
+
+  def boolean(true_ratio, integer: false) when true_ratio in 0..100, do: boolean()
 end

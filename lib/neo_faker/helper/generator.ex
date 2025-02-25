a79014bbe58a6bd46d633480b6758c933a2d3bdc
muzhawir <muzhawir.amri@gmail.com>
2025-02-25 13:19:53 +0800

    Add functions comment in NeoFaker.Helper.Generator

--- lib/neo_faker/helper/generator.ex
+++ lib/neo_faker/helper/generator.ex
@@ -3,6 +3,12 @@ defmodule NeoFaker.Helper.Generator do
 
   alias NeoFaker.Helper.Locale
 
+  @doc """
+  Returns a random value.
+
+  Return a random value from specified locale file.
+  """
+  @spec random(atom(), String.t(), String.t(), Keyword.t()) :: String.t()
   def random(module, file, key, opts \\ []) do
     module
     |> fetch_data(file, opts)
@@ -10,6 +16,13 @@ defmodule NeoFaker.Helper.Generator do
     |> Enum.random()
   end
 
+  @doc """
+  Fetches data from the persistent term.
+
+  Fetches data from the persistent term, if the data is not found, it will be loaded from the
+  locale file first.
+  """
+  @spec fetch_data(atom(), String.t(), Keyword.t()) :: map()
   def fetch_data(module, file, opts \\ []) do
     opts |> Keyword.get(:locale, "default") |> Locale.load_persistent_term(module, file)
   end

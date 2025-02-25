86af7c6f67c86b2fa112b3c6383088e3157e46f2
muzhawir <muzhawir.amri@gmail.com>
2025-02-25 16:06:21 +0800

    Refactor NeoFaker.Person.Utils

--- lib/neo_faker/person/utils.ex
+++ lib/neo_faker/person/utils.ex
@@ -1,69 +1,47 @@
 defmodule NeoFaker.Person.Utils do
   @moduledoc false
 
-  import NeoFaker.Helper.Generator, only: [random: 4]
+  import NeoFaker.Helper.Generator, only: [fetch_data: 3, random: 4]
+
+  alias NeoFaker.Helper.Locale
 
   @female_name_file "female_name.exs"
   @male_name_file "male_name.exs"
 
-  # @unisex_name_file "unisex_name.exs"
-  # @masculine_name_file "masculine_name.exs"
-  # @feminine_name_file "feminine_name.exs"
-  # @persistent_data "neo_faker_person_all_names"
-
   @doc """
   Returns a random name.
+
+  Returns a random name, if no options are provided it will return a random unisex name.
   """
   @spec random_name(atom(), String.t(), Keyword.t()) :: String.t()
   def random_name(module, key, opts \\ []) do
-    file =
-      case Keyword.get(opts, :sex, :female) do
-        :female -> @female_name_file
-        :male -> @male_name_file
-      end
-
-    random(module, file, key, opts)
+    case Keyword.get(opts, :sex) do
+      nil -> random_unisex_name(module, key, opts)
+      :female -> random(module, @female_name_file, key, opts)
+      :male -> random(module, @male_name_file, key, opts)
+    end
   end
 
-  @doc """
-  Returns a list of all random names.
+  # Merges the male and female data names and returns a random unisex name
+  defp random_unisex_name(module, key, opts) do
+    locale = Keyword.get(opts, :locale) || Application.get_env(:neo_faker, :locale)
+    male_names = fetch_data(module, @male_name_file, locale: locale)
+    female_names = fetch_data(module, @female_name_file, locale: locale)
+    persisent_term_key = Locale.persistent_term_key(locale, module, "unisex_name")
 
-  Returns a list of all random names, if list is not yet existing in persistent term it will be created.
-  """
-  # @spec load_all_random_names(atom(), String.t()) :: list()
-  # def load_all_random_names(module, key) do
-  #   case :persistent_term.get("neo_faker_person_all_names", nil) do
-  #     nil ->
-  #       store_all_names(module)
+    unisex_names =
+      Map.merge(male_names, female_names, fn _key, male, female ->
+        male |> Stream.concat(female) |> Enum.shuffle()
+      end)
 
-  #       @persistent_data |> :persistent_term.get() |> Map.get(key, [])
+    case :persistent_term.get(persisent_term_key, nil) do
+      nil ->
+        :persistent_term.put(persisent_term_key, unisex_names)
 
-  #     all_names ->
-  #       Map.get(all_names, key, [])
-  #   end
-  # end
+        persisent_term_key |> :persistent_term.get() |> Map.get(key) |> Enum.random()
 
-  # Stores all random names in persistent term.
-  #
-  # Stores combined random unisex, masculine, and feminine names in persistent term.
-  # defp store_all_names(module) do
-  #   all_names = %{
-  #     "first_names" => combine_names_list(module, "first_names"),
-  #     "middle_names" => combine_names_list(module, "middle_names"),
-  #     "last_names" => combine_names_list(module, "last_names")
-  #   }
-
-  #   :persistent_term.put(@persistent_data, all_names)
-  # end
-
-  # Combines unisex, masculine, and feminine names into one list.
-  # defp combine_names_list(module, key) do
-  #   all_names = [
-  #     read_persistent_term(module, @unisex_name_file, key),
-  #     read_persistent_term(module, @masculine_name_file, key),
-  #     read_persistent_term(module, @feminine_name_file, key)
-  #   ]
-
-  #   all_names |> List.flatten() |> Enum.shuffle()
-  # end
+      term ->
+        term |> Map.get(key) |> Enum.random()
+    end
+  end
 end

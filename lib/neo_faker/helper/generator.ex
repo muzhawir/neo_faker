bd4210f00aa00c16fbc19756a3e2676c861e5d5c
muzhawir <muzhawir.amri@gmail.com>
2025-02-26 22:47:22 +0800

    Refactor NeoFaker.Helper.Locale

--- lib/neo_faker/helper/generator.ex
+++ lib/neo_faker/helper/generator.ex
@@ -4,9 +4,22 @@ defmodule NeoFaker.Helper.Generator do
   alias NeoFaker.Helper.Locale
 
   @doc """
-  Returns a random value.
+  Retrieves a random value from the specified locale data file.
 
-  Return a random value from specified locale file.
+  This function fetches a random value from a locale-specific data file. A key can be specified
+  to extract a particular value from the map.
+
+  ## Parameters
+
+  - `module` - The module where the locale data is stored, e.g., `NeoFaker.App`.
+  - `file` - The name of the data file within the module, e.g., `"author.exs"`.
+  - `key` - The specific key within the data file, e.g., `"first_names"`, `"last_names"`.
+  - `opts` - Additional options (optional).
+
+  ### Options
+
+  - `:locale` - Specifies the locale to use, e.g., `"id_id"`. If not provided, `nil` will be used,
+  which loads the default locale.
   """
   @spec random(atom(), String.t(), String.t(), Keyword.t()) :: String.t()
   def random(module, file, key, opts \\ []) do
@@ -17,10 +30,9 @@ defmodule NeoFaker.Helper.Generator do
   end
 
   @doc """
-  Fetches data from the persistent term.
+  Retrieves data from the persistent term.
 
-  Fetches data from the persistent term, if the data is not found, it will be loaded from the
-  locale file first.
+  If the requested data is not found, it will be loaded from the corresponding locale file first.
   """
   @spec fetch_data(atom(), String.t(), Keyword.t()) :: map() | list()
   def fetch_data(module, file, opts \\ []) do
@@ -30,11 +42,12 @@ defmodule NeoFaker.Helper.Generator do
     load_data(locale, module, file, key)
   end
 
-  # Load data from the persistent term
+  # Loads data from the persistent term if no cached data is available.
   defp load_data(locale, module, file, nil), do: Locale.load_persistent_term(locale, module, file)
 
-  # Load data from the persistent term and return the specified key
+  # Loads data from the persistent term and retrieves the specified key.
+  # If the key is not found, it returns `nil`.
   defp load_data(locale, module, file, key) do
-    locale |> Locale.load_persistent_term(module, file) |> Map.fetch!(key)
+    locale |> Locale.load_persistent_term(module, file) |> Map.get(key)
   end
 end
--- lib/neo_faker/helper/locale.ex
+++ lib/neo_faker/helper/locale.ex
@@ -7,10 +7,17 @@ defmodule NeoFaker.Helper.Locale do
   @data_path Path.join([File.cwd!(), "lib", "data"])
 
   @doc """
-  Loads data from the persistent term.
+  Loads data from the persistent term or falls back to the locale file.
 
-  Loads data from the persistent term, if the data is not found in the persistent term, it will be
-  loaded from the locale file first.
+  Attempts to retrieve data from the persistent term. If the data is not found,
+  it will be loaded from the corresponding locale file and stored in the persistent term
+  for future access.
+
+  ## Parameters
+
+  - `locale` - The locale identifier (e.g., `"id_id"`, `"en_us"`).
+  - `module` - The module associated with the locale data (e.g., `NeoFaker.App`).
+  - `file` - The name of the data file within the module (e.g., `"author.exs"`).
   """
   @spec load_persistent_term(locale(), atom(), String.t()) :: any()
   def load_persistent_term(locale \\ nil, module, file) do
@@ -28,9 +35,16 @@ defmodule NeoFaker.Helper.Locale do
   end
 
   @doc """
-  Loads data from the locale file and stores it in persistent term.
+  Loads data from the locale file and stores it in the persistent term.
 
-  Reads the specified data from the locale file and stores it in the persistent term.
+  Reads the specified data from the locale file and caches it in the persistent term
+  for efficient future access.
+
+  ## Parameters
+
+  - `locale` - The locale identifier (e.g., `"id_id"`, `"en_us"`).
+  - `module` - The module associated with the locale data (e.g., `NeoFaker.App`).
+  - `file` - The name of the data file within the module (e.g., `"author.exs"`).
   """
   @spec store_persistent_term(locale(), atom(), String.t()) :: :ok
   def store_persistent_term(locale \\ nil, module, file) do
@@ -41,9 +55,10 @@ defmodule NeoFaker.Helper.Locale do
   end
 
   @doc """
-  Creates a key for storing data in persistent term.
+  Constructs a unique key for storing data in the persistent term.
 
-  Creates a key for storing data in persistent term based on the locale, module, and file name.
+  Generates a deterministic key based on the given `locale`, `module`, and `file`
+  name. The key is used to store and retrieve data efficiently from the persistent term.
   """
   @spec persistent_term_key(locale(), atom(), String.t()) :: String.t()
   def persistent_term_key(locale \\ nil, module, file) do
@@ -60,9 +75,10 @@ defmodule NeoFaker.Helper.Locale do
   end
 
   @doc """
-  Reads data from the locale file.
+  Reads and returns data from the specified locale file.
 
-  Reads the specified data from the locale file and returns it as a map.
+  Reads the data from a locale-specific file and returns it as a map.
+  If the file cannot be found or fails to load, an exception will be raised.
   """
   @spec read_locale_file!(locale(), atom(), String.t()) :: locale_data()
   def read_locale_file!(locale \\ nil, module, file) do
@@ -81,9 +97,9 @@ defmodule NeoFaker.Helper.Locale do
   end
 
   @doc """
-  Returns the name of the current module in lowercase.
+  Returns the lowercase name of the current module.
 
-  Splits the module name into parts and returns the last part in lowercase string.
+  Extracts the last part of the module name, converts it to a lowercase string, and returns it.
   """
   @spec current_module(atom()) :: String.t()
   def current_module(module) do
@@ -94,10 +110,14 @@ defmodule NeoFaker.Helper.Locale do
   end
 
   @doc """
-  Returns the path to the locale file.
+  Returns the file path for the specified locale.
 
-  If the locale is provided, it returns the path to the locale file. Otherwise, it returns the path
-  to the default locale file.
+  This function constructs the path to the locale file based on the given locale. If no locale
+  is provided, it defaults to the path of the default locale file.
+
+  ## Parameters
+
+  - `locale` - The locale identifier (e.g., `"id_id"`, `"en_us"`).
   """
   @spec locale_path(locale()) :: String.t()
   def locale_path(locale \\ nil) do

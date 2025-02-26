5403689196a13077844e230c2145b7a3732bd385
muzhawir <muzhawir.amri@gmail.com>
2025-02-26 23:00:49 +0800

    Update config comment

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,7 @@
 import Config
 
-# Configure NeoFaker to use the default locale. Available locales can be found at
-# https://hexdocs.pm/neo_faker/available-locales.html
+# Sets the default locale for NeoFaker.
+# This determines the language and regional data used by the library.
+# Available locales can be found at: https://hexdocs.pm/neo_faker/available-locales.html
 
 config :neo_faker, locale: "default"

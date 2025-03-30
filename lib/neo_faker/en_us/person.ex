c223596131090243a14cc5356a4fb76b7f9935a2
muzhawir <muzhawir.amri@gmail.com>
2025-03-30 12:13:04 +0800

    Add new module NeoFaker.EnUs.Person

--- lib/neo_faker/en_us/person.ex
+++ lib/neo_faker/en_us/person.ex
@@ -1,13 +1,39 @@
 defmodule NeoFaker.EnUs.Person do
   @moduledoc """
-  Testing EN US generator
+  Provides functions for generating person-related information specific to the United States
+  locale.
+
+  This module offers a variety of functions to generate random personal details specific to the
+  United States, such as Social Security Numbers (SSNs), among others.
   """
-  import NeoFaker.Helper.Generator
+
+  alias NeoFaker.Number
 
   @doc """
-  should return a random first name
+  Generates a random SSN.
+
+  Returns a random SSN (Social Security Number).
+
+  ## Examples
+
+      iex> NeoFaker.EnUs.Person.ssn()
+      "184-63-2006"
+
   """
-  def first_name do
-    random(NeoFaker.EnUs.Person, "gender.exs", "non_binary")
+  def ssn do
+    group_number = 1 |> Number.between(99) |> to_string() |> String.pad_leading(2, "0")
+    serial_number = 1 |> Number.between(9999) |> to_string() |> String.pad_leading(4, "0")
+
+    "#{area_number()}-#{group_number}-#{serial_number}"
+  end
+
+  # Generate a random area number, if the area number is 666, generate a new area number.
+  defp area_number do
+    number = Number.between(1, 899)
+
+    case number do
+      666 -> 1 |> Number.between(899) |> to_string() |> String.pad_leading(3, "0")
+      _ -> number |> to_string() |> String.pad_leading(3, "0")
+    end
   end
 end

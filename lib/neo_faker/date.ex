a5ae9575a809a86fb7a531ee34857349f94b5c53
muzhawir <muzhawir.amri@gmail.com>
2025-04-01 12:42:52 +0800

    Add NeoFaker.Date.between/3

--- lib/neo_faker/date.ex
+++ lib/neo_faker/date.ex
@@ -28,14 +28,50 @@ defmodule NeoFaker.Date do
       iex> NeoFaker.Date.add()
       ~D[2025-03-25]
 
-      iex> NeoFaker.Date.add(format: :iso8601)
+      iex> NeoFaker.Date.add(0..31)
+      ~D[2025-03-30]
+
+      iex> NeoFaker.Date.add(0..31, format: :iso8601)
       "2025-03-25"
 
   """
   @spec add(Range.t(), Keyword.t()) :: Date.t() | String.t()
   def add(range \\ -365..365, opts \\ []) do
-    type = Keyword.get(opts, :format, :date)
+    format = Keyword.get(opts, :format, :date)
 
-    random_date(range, type)
+    random_add_date(range, format)
+  end
+
+  @doc """
+  Generates a random date between two dates.
+
+  Returns a date between the specified start and finish dates.
+
+  ## Options
+
+  - `:format` - Specifies the format of the date.
+
+  The values for `:format` can be:
+
+  - `:date` - Returns the date in `Date` format (default).
+  - `:iso8601` - Returns the date in ISO 8601 format.
+
+  ## Examples
+
+      iex> NeoFaker.Date.between()
+      ~D[2025-03-25]
+
+      iex> NeoFaker.Date.between(~D[2020-01-01], ~D[2025-01-01])
+      ~D[2022-08-17]
+
+      iex> NeoFaker.Date.between(~D[2025-03-25], ~D[2025-03-25], format: :iso8601)
+      "2025-03-25"
+
+  """
+  @spec between(Date.t(), Date.t(), Keyword.t()) :: Date.t() | String.t()
+  def between(start \\ ~D[1970-01-01], finish \\ Date.utc_today(), opts \\ []) do
+    format = Keyword.get(opts, :format, :date)
+
+    random_between_date(start, finish, format)
   end
 end
--- test/neo_faker/date_test.exs
+++ test/neo_faker/date_test.exs
@@ -12,4 +12,18 @@ defmodule NeoFaker.DateTest do
       assert FakeDate.add(0..0, format: :iso8601) == Date.to_iso8601(Date.utc_today())
     end
   end
+
+  describe "between/3" do
+    test "returns a random date in Date format" do
+      today = Date.utc_today()
+
+      assert FakeDate.between(today, today) == today
+    end
+
+    test "returns a random date in ISO 8601 format" do
+      today = Date.utc_today()
+
+      assert FakeDate.between(today, today, format: :iso8601) == Date.to_iso8601(today)
+    end
+  end
 end

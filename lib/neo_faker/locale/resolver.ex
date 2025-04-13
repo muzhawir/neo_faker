df84ed4f35b894d183d307bba06df5dd7e418af6
muzhawir <muzhawir.amri@gmail.com>
2025-04-13 15:46:40 +0800

    Add NeoFaker.Locale.Resolver

---
+++ lib/neo_faker/locale/resolver.ex
@@ -0,0 +1,34 @@
+defmodule NeoFaker.Locale.Resolver do
+  @moduledoc false
+
+  alias NeoFaker.Locale.Disk
+
+  @data_path Path.join([File.cwd!(), "lib", "data"])
+  @locale_file Path.join([@data_path, "locale.exs"])
+
+  @doc """
+  Resolves a locale based on the given locale.
+  """
+  @spec resolve_locale(atom()) :: atom()
+  def resolve_locale(locale) do
+    if locale_exists?(locale) do
+      locale
+    else
+      :default
+    end
+  end
+
+  @doc """
+  Checks if a locale exists in the locale.exs file.
+  """
+  @spec locale_exists?(atom()) :: boolean()
+  def locale_exists?(locale) do
+    if is_nil(:persistent_term.get(:available_locales, nil)) do
+      :persistent_term.put(:available_locales, MapSet.new(Disk.evaluate_file!(@locale_file)))
+
+      :available_locales |> :persistent_term.get() |> MapSet.member?(Atom.to_string(locale))
+    else
+      :available_locales |> :persistent_term.get() |> MapSet.member?(Atom.to_string(locale))
+    end
+  end
+end

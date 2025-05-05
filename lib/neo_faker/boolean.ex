60bdb5e782787a1362bb09e422e2645ac196fa2a
muzhawir <muzhawir.amri@gmail.com>
2025-05-05 11:29:24 +0800

    Fix NeoFaker.Boolean.boolean

--- lib/neo_faker/boolean.ex
+++ lib/neo_faker/boolean.ex
@@ -30,16 +30,14 @@ defmodule NeoFaker.Boolean do
   @spec boolean(1..100, Keyword.t()) :: boolean() | non_neg_integer()
   def boolean(true_ratio \\ 50, opts \\ [])
 
-  def boolean(true_ratio, []) when true_ratio in 0..100 do
-    :rand.uniform() <= true_ratio / 100
-  end
-
-  def boolean(true_ratio, integer: false) when true_ratio in 0..100, do: boolean()
-
-  def boolean(true_ratio, integer: true) when true_ratio in 0..100 do
+  def boolean(true_ratio, opts) when true_ratio in 0..100 do
     result = :rand.uniform() <= true_ratio / 100
 
-    if result == true, do: 1, else: 0
+    if Keyword.get(opts, :integer, false) do
+      if result, do: 1, else: 0
+    else
+      result
+    end
   end
 
   def boolean(true_ratio, _opts) when true_ratio not in 0..100 do

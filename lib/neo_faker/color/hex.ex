81213b1c79a752f7f399b1c11a9058f5554f1b11
muzhawir <muzhawir.amri@gmail.com>
2025-07-18 19:14:01 +0800

    Refactor and add guard in NeoFaker.Color.HEX.color/1

--- lib/neo_faker/color/hex.ex
+++ lib/neo_faker/color/hex.ex
@@ -1,14 +1,14 @@
 defmodule NeoFaker.Color.HEX do
   @moduledoc false
 
-  @hex_digits Enum.shuffle(~w[0 1 2 3 4 5 6 7 8 9 A B C D E F])
+  @hex_digits ~w[0 1 2 3 4 5 6 7 8 9 A B C D E F]
 
   @doc """
   Generates a random HEX color string of the specified length.
 
-  The resulting string consists of randomly selected hexadecimal digits (0-9, a-f).
+  The resulting string consists of randomly selected hexadecimal digits (0-9, A-F).
   """
-  def color(digits) do
+  def color(digits) when digits in [3, 4, 6, 8] do
     Enum.map_join(1..digits, fn _ -> Enum.random(@hex_digits) end)
   end
 end

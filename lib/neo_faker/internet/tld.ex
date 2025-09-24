927596be2cee6d415b244bad7c880bbae7a79145
muzhawir <muzhawir.amri@gmail.com>
2025-09-24 12:44:38 +0800

    Refactor TLD module to enhance type definition and improve documentation for name function

--- lib/neo_faker/internet/tld.ex
+++ lib/neo_faker/internet/tld.ex
@@ -4,12 +4,26 @@ defmodule NeoFaker.Internet.TLD do
   alias NeoFaker.Data.Cache
   alias NeoFaker.Data.Generator
 
+  @type tld_type :: :all | :all_except_safe | :safe | :generic | :sponsored | :country_code
+
   @module NeoFaker.Internet
 
   @tld_file "tld.exs"
 
+  @doc """
+  Generates a random top-level domain (TLD) based on the specified type.
+  """
+  @spec name(tld_type()) :: String.t()
   def name(type) do
     case type do
+      :all_except_safe ->
+        :default
+        |> Cache.fetch!(@module, @tld_file)
+        |> Map.delete("safe")
+        |> Map.values()
+        |> List.flatten()
+        |> Enum.random()
+
       :all ->
         :default
         |> Cache.fetch!(@module, @tld_file)

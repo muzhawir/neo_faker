128ef951171525966639d9f3fa5180fd9f2f8418
muzhawir <muzhawir.amri@gmail.com>
2025-09-24 12:45:02 +0800

    Add user_name function to generate random usernames with customizable options

--- lib/neo_faker/internet.ex
+++ lib/neo_faker/internet.ex
@@ -8,6 +8,7 @@ defmodule NeoFaker.Internet do
   @moduledoc since: "0.13.0"
 
   alias NeoFaker.Internet.TLD
+  alias NeoFaker.Internet.UserName
 
   @doc """
   Generates a random top-level domain (TLD).
@@ -23,7 +24,8 @@ defmodule NeoFaker.Internet do
 
   The values for `:type` can be:
 
-  - `:all` - Returns a TLD from all available types (default).
+  - `:all_except_safe` - Returns a TLD from all types except safe TLDs. This is the default.
+  - `:all` - Returns a TLD from all available types, including safe TLDs.
   - `:safe` - Returns a safe TLD, e.g. `.example`.
   - `:generic` - Returns a generic TLD, e.g. `.com`.
   - `:sponsored` - Returns a sponsored TLD, e.g. `.edu`.
@@ -46,7 +48,7 @@ defmodule NeoFaker.Internet do
   """
   @spec tld(Keyword.t()) :: String.t()
   def tld(opts \\ []) do
-    type = Keyword.get(opts, :type, :all)
+    type = Keyword.get(opts, :type, :all_except_safe)
 
     if Keyword.get(opts, :dot, true) do
       "." <> TLD.name(type)
@@ -54,4 +56,64 @@ defmodule NeoFaker.Internet do
       TLD.name(type)
     end
   end
+
+  @doc """
+  Generates a random username.
+
+  Returns a random username string.
+
+  ## Options
+
+  The accepted options are:
+
+  - `:word_count` - Specifies the number of words to include in the username. Defaults to `2`.
+  - `:separator` - Defines the separator to use between words in the username.
+  - `:username_type` - Specifies the type of words to use in the username.
+  - `:number` - A boolean indicating whether to append a random number to the username.
+     Defaults to `false`.
+  - `:number_range` - Defines the range of numbers to choose from when appending a number.
+     Defaults to `1..1000`.
+
+  The values for `:separator` can be:
+
+  - `:all` - Uses any of the separators (default).
+  - `:dot` - Uses a dot (`.`) as the separator.
+  - `:underscore` - Uses an underscore (`_`) as the separator.
+  - `:dash` - Uses a dash (`-`) as the separator.
+
+  The values for `:username_type` can be:
+
+  - `:person` - Uses random first or last names for the username, which is the default.
+  - `:word` - Uses random words for the username.
+
+  ## Examples
+
+      iex> NeoFaker.Internet.user_name()
+      "josé_valim"
+
+      iex> NeoFaker.Internet.user_name(word_count: 3, separator: :dot)
+      "abigail.bethany.crawford"
+
+      iex> NeoFaker.Internet.user_name(username_type: :word, number: true, number_range: 1..2025)
+      "elixir_alchemist_2012"
+
+  """
+  @spec user_name(Keyword.t()) :: String.t()
+  def user_name(opts \\ []) do
+    word_count = 1..Keyword.get(opts, :word_count, 2)
+    separator = opts |> Keyword.get(:separator, :all) |> UserName.separator()
+
+    base =
+      Enum.map_join(word_count, separator, fn _ ->
+        UserName.word(Keyword.get(opts, :username_type, :person))
+      end)
+
+    if Keyword.get(opts, :number, false) do
+      base <> separator <> "#{Enum.random(Keyword.get(opts, :number_range, 1..1000))}"
+    else
+      base
+    end
+  end
+
+  # def popular_email_domain, do: Generator.random_value(__MODULE__, "email_provider.exs", "domain")
 end
--- test/neo_faker/internet_test.exs
+++ test/neo_faker/internet_test.exs
@@ -24,4 +24,41 @@ defmodule NeoFaker.InternetTest do
       end)
     end
   end
+
+  describe "user_name/1" do
+    test "returns a user_name with the specified word count" do
+      user_name = Internet.user_name(word_count: 3)
+
+      assert user_name |> String.split([".", "-", "_"]) |> length() == 3
+    end
+
+    test "returns a user_name with the specified separator" do
+      Enum.each(%{dot: ".", underscore: "_", dash: "-"}, fn {key, val} ->
+        username = Internet.user_name(separator: key)
+
+        assert String.contains?(username, val)
+      end)
+
+      assert [separator: :all] |> Internet.user_name() |> String.contains?([".", "_", "-"])
+    end
+
+    test "returns a user_name with the specified username type" do
+      Enum.each([:person, :word], fn type ->
+        user_name = Internet.user_name(username_type: type)
+
+        assert String.contains?(user_name, [".", "-", "_"]) && String.valid?(user_name)
+      end)
+    end
+
+    test "returns a user_name with appended number and specific range" do
+      extracted_number =
+        [number: true, number_range: 100..200]
+        |> Internet.user_name()
+        |> String.split([".", "-", "_"])
+        |> List.last()
+        |> String.to_integer()
+
+      assert extracted_number in 100..200
+    end
+  end
 end

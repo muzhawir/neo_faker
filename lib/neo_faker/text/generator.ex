0568254d1ce44fcd6495c11a52f6efdaf4e7e4cd
muzhawir <muzhawir.amri@gmail.com>
2026-03-11 19:34:42 +0800

    Remove Enum import and add alphabet attrs

--- lib/neo_faker/text/generator.ex
+++ lib/neo_faker/text/generator.ex
@@ -1,11 +1,12 @@
 defmodule NeoFaker.Text.Generator do
   @moduledoc false
 
-  import Enum, only: [random: 1, shuffle: 1]
-
   @alphabet_lower ~w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
   @alphabet_upper ~w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
   @digits ~w[0 1 2 3 4 5 6 7 8 9]
+
+  @alphabet @alphabet_lower ++ @alphabet_upper
+  @alphanumeric @alphabet ++ @digits
 
   @doc """
   Generates a random character based on the specified type.
@@ -17,9 +18,9 @@ defmodule NeoFaker.Text.Generator do
   - `:digit` - returns a random digit character.
   """
   @spec character(atom() | nil) :: String.t()
-  def character(nil), do: random(shuffle(@alphabet_lower ++ @alphabet_upper ++ @digits))
-  def character(:alphabet_lower), do: random(shuffle(@alphabet_lower))
-  def character(:alphabet_upper), do: random(shuffle(@alphabet_upper))
-  def character(:alphabet), do: random(shuffle(@alphabet_lower ++ @alphabet_upper))
-  def character(:digit), do: random(shuffle(@digits))
+  def character(nil), do: Enum.random(@alphanumeric)
+  def character(:alphabet_lower), do: Enum.random(@alphabet_lower)
+  def character(:alphabet_upper), do: Enum.random(@alphabet_upper)
+  def character(:alphabet), do: Enum.random(@alphabet)
+  def character(:digit), do: Enum.random(@digits)
 end
